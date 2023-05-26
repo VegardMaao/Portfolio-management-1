52c85eb6e6ef10670be99024f055b9af1512123d
Vegard M <vm932648@gmail.com>
2023-05-26 12:31:23 +0200

    Update navigaitonObjects.js

--- js/exports/objects/navigaitonObjects.js
+++ js/exports/objects/navigaitonObjects.js
@@ -1,10 +1,10 @@
 export const navItems = [{
-    title: "Portfolio",
-    link: "index.html#portfolio"
-}, {
     title: "About me",
     link: "index.html#about"
-}, {
+},{
+    title: "Portfolio",
+    link: "index.html#portfolio"
+},{
     title: "Contact",
     link: "index.html#contact"
 }];
