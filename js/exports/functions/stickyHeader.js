28bbc25b52ed75828d5c4e0ab44e9e80f2f93474
Vegard M <vm932648@gmail.com>
2023-05-26 12:30:00 +0200

    changing header
    changing header and trying to make a sticky header function.
    
    The header looks neater now, but I  got to do more work on the sticky header function for it to  work.
    
    I also have to make a function that closes the header if I press an item  inside of the menu  (on mobile)

---
+++ js/exports/functions/stickyHeader.js
@@ -0,0 +1,9 @@
+// export function fixedNav(header, pos) {
+//     if (window.scrollY >= pos) {    
+//         console.log("hello")
+//         header.classList.add('sticky');
+//     } else {
+//         console.log("goodbye")
+//         header.classList.remove('sticky');    
+//     }
+//   }
--- js/main-script.js
+++ js/main-script.js
@@ -1,16 +1,21 @@
 import { createNavigation } from "./exports/functions/createNavigation.js";
 import { navItems } from "./exports/objects/navigaitonObjects.js";
 import { footerItems } from "./exports/objects/footerObjects.js";
-
 const navContainer = document.querySelector(".nav_ul");
 const footerContainer = document.querySelector(".cs_ul");
-
 createNavigation(navContainer, navItems);
 createNavigation(footerContainer, footerItems);
 
 import { createPortfolio } from "./exports/functions/createPortfolio.js";
 import { portfolioItems } from "./exports/objects/portfolioObjects.js";
-
 const portfolioContainer = document.querySelector(".portfolio-container");
-
 createPortfolio(portfolioContainer, portfolioItems);
+
+// import  { fixedNav } from "./exports/functions/stickyHeader.js"
+// const header = document.querySelector("header");
+// let position = header.offsetTop;
+// window.addEventListener("scroll", e => {
+//     console.log(position) 
+//     console.log(window.scrollY)
+//     fixedNav(header, position)
+// });
