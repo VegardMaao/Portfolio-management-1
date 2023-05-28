e317d5339a21fed941578f74b7372fc2057bcea6
Vegard M <vm932648@gmail.com>
2023-05-28 13:11:28 +0200

    sticky header

--- js/main-script.js
+++ js/main-script.js
@@ -11,15 +11,6 @@ import { portfolioItems } from "./exports/objects/portfolioObjects.js";
 const portfolioContainer = document.querySelector(".portfolio-container");
 createPortfolio(portfolioContainer, portfolioItems);
 
-// import  { fixedNav } from "./exports/functions/stickyHeader.js"
-// const header = document.querySelector("header");
-// let position = header.offsetTop;
-// window.addEventListener("scroll", e => {
-//     console.log(position) 
-//     console.log(window.scrollY)
-//     fixedNav(header, position)
-// });
-
 import { closeMenu } from "./exports/functions/closeMenuOnClick.js";
 const menuCheckBox = document.querySelector("#hamburger_menu");
 const menuIcon =  document.querySelector("#menu_icon");
