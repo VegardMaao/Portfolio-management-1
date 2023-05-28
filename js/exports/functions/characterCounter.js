aace4d741bd2042091949fe873b17b4e0eb6e6a2
Vegard M <vm932648@gmail.com>
2023-05-28 13:02:03 +0200

    Minor improvements to design
    Minor improvements to design and removing comments

--- js/exports/functions/characterCounter.js
+++ js/exports/functions/characterCounter.js
@@ -2,7 +2,7 @@ export function characterCount(input, counter, maxValText, maxLength, minVal) {
     const inpLength = input.length;
     counter.innerText = inpLength;
     maxValText.innerText = maxLength;
-    if (inpLength <= minVal || inpLength >= maxLength){
+    if (inpLength < minVal || inpLength >= maxLength){
         counter.style.cssText = "color: red;"
     } else {
         counter.style.cssText ="color: #fff;"
