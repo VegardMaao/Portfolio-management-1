e7b64c966f7858d09c33da33c9710867b5cac327
Vegard M <vm932648@gmail.com>
2023-05-28 14:03:11 +0200

    Update portfolioObjects.js
    changing links from #readme to regular github - end  users who click  this link probably know where to look

--- js/exports/objects/portfolioObjects.js
+++ js/exports/objects/portfolioObjects.js
@@ -2,18 +2,18 @@ export const portfolioItems = [{
     title: "Project Exam 1",
     imageUrl: "/images/loppasbigblog.jpg",
     description: "This is the most recent project that I have made, and the one most representative of my current skillset. The site was made using vanilla HTML, CSS and JS, as well as the WordPress REST API to manage content.",
-    gitHubLink: "https://github.com/Noroff-FEU-Assignments/project-exam-1-VegardMaao#readme",
+    gitHubLink: "https://github.com/Noroff-FEU-Assignments/project-exam-1-VegardMaao",
     websiteLink: "https://loppasbigblog.netlify.app/"
 },{
     title: "Cross-Course Project",
     imageUrl: "/images/cross-course.jpg",
     description: "The Cross-Course Project was made as part of several assignments at Noroff, all the way from design and basic HTML/CSS to dynamically creating pages using JS and the WP REST API (managing content using WooCommerce).",
-    gitHubLink: "https://github.com/VegardMaao/html-and-css-CA_Vegard-Maaoe#readme",
+    gitHubLink: "https://github.com/VegardMaao/html-and-css-CA_Vegard-Maaoe",
     websiteLink: "https://ecstatic-jackson-e63b3f.netlify.app/"
 }, {
     title: "Semester Project 2022",
     imageUrl: "/images/comscimuseum.jpg",
     description: "This was a fun project! The assignment was making a site for a childrens science museum, using basic HTML and CSS. I added some JavaSCript, but it was not part of the assignment. The JS isn't good, but I learned a lot.",
-    gitHubLink: "https://github.com/VegardMaao/semester_project_spring22_vegard_maao#readme",
+    gitHubLink: "https://github.com/VegardMaao/semester_project_spring22_vegard_maao",
     websiteLink: "https://comscimuseum.netlify.app/"
 }]
