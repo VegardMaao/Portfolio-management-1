e1e8517da1e8941e0fd847baafe94465d7b280fe
Vegard M <vm932648@gmail.com>
2023-05-28 13:50:41 +0200

    Delete stickyHeader.js

--- js/exports/functions/stickyHeader.js
+++
@@ -1,9 +0,0 @@
-// export function fixedNav(header, pos) {
-//     if (window.scrollY >= pos) {    
-//         console.log("hello")
-//         header.classList.add('sticky');
-//     } else {
-//         console.log("goodbye")
-//         header.classList.remove('sticky');    
-//     }
-//   }
