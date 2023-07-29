b9d5444d8218db90b21c533c2230d6217a31f7cf
Vegard M <vm932648@gmail.com>
2023-07-29 09:09:35 +0200

    UX improvement
    Opening GitHub and LinkedIn profiles in new tabs, rather than the same tab as my portfolio

--- js/exports/functions/createNavigation.js
+++ js/exports/functions/createNavigation.js
@@ -1,5 +1,5 @@
 export function createNavigation(container, array) {
     for (let i = 0; i < array.length; i++) {
-        container.innerHTML += `<li><a href="${array[i].link}">${array[i].title}</a></li>`;
+        container.innerHTML += `<li><a href="${array[i].link}" target="${array[i].target}">${array[i].title}</a></li>`;
     }
 }
--- js/exports/objects/navigaitonObjects.js
+++ js/exports/objects/navigaitonObjects.js
@@ -1,10 +1,13 @@
 export const navItems = [{
     title: "About me",
-    link: "index.html#about"
+    link: "index.html#about",
+    target: "_self"
 },{
     title: "Portfolio",
-    link: "index.html#portfolio"
+    link: "index.html#portfolio",
+    target: "_self"
 },{
     title: "Contact",
-    link: "index.html#contact"
+    link: "index.html#contact",
+    target: "_self"
 }];
