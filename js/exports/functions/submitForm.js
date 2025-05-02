05806aeb28c8b7ea79a8c830294c1566c0c2e1a7
Vegard M. <vm932648@gmail.com>
2025-05-02 12:15:44 +0200

    Update submitForm.js

--- js/exports/functions/submitForm.js
+++ js/exports/functions/submitForm.js
@@ -1,15 +1,16 @@
 export async function submitForm(e, inputs, url, container, message) {
-    e.preventDefault();
-    const dataInFormDataFormat = new FormData(inputs);
-    try {
-        container.innerHTML = `<div class="comment-posted">${message}</div>`;
-        const fetchResponse = await fetch(url, {
-            method: "post",
-            body: dataInFormDataFormat
-        })
-        const finishedResponse = await fetchResponse.json();
-        return finishedResponse;
-    } catch (error) {
-        container.innerHTML = `<div class="comment-posted"><p>Hmm, something went wrong. It's ${error}</p></div>`;
-    }
+  e.preventDefault();
+  const dataInFormDataFormat = new FormData(inputs);
+  dataInFormDataFormat.append("_wpcf7_unit_tag", "f0a0ed8");
+  try {
+    container.innerHTML = `<div class="comment-posted">${message}</div>`;
+    const fetchResponse = await fetch(url, {
+      method: "post",
+      body: dataInFormDataFormat,
+    });
+    const finishedResponse = await fetchResponse.json();
+    return finishedResponse;
+  } catch (error) {
+    container.innerHTML = `<div class="comment-posted"><p>Hmm, something went wrong. It's ${error}</p></div>`;
+  }
 }
